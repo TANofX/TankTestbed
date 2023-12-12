c1aca91cda94d7320de897f4c5764659463770fa
JeXuan Lian Liu <Lianliuj1@ccsdbraves.org>
2023-12-12 18:50:44 -0500

    initial

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -13,12 +22,36 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+    private final TalonFX m_leftMotor1 = new TalonFX(0);
+    private final TalonFX m_rightMotor1 = new TalonFX(1);
+    private final TalonFX m_leftMotor2 = new TalonFX(3);
+    private final TalonFX m_rightMotor2 = new TalonFX(4);
+
+    private MotorControllerGroup leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2)
+        private MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
+
+
+    private final DifferentialDrive drives = new DifferentialDrive(leftMotors, rightMotors);
+
+    private final XboxController controller = new XboxController(0);
+
+    private Joystick verticalStick = new Joystick(0);
+    private Joystick horizontalStick = new Joystick(1);
+
+    
+   
+    //private final Timer m_timer = new Timer();
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    rightMotors.setInverted(true);
+
+   
+     
+  }
 
   @Override
   public void robotPeriodic() {}
@@ -33,7 +66,23 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    //read button and joystick/solenoid
+    
+      drives.arcadeDrive(verticalStick.getY(), 0);
+   
+    
+      drives.arcadeDrive(verticalStick.getY(), 0);
+
+  
+    drives.arcadeDrive(0, horizontalStick.getX() );
+
+
+  drives.arcadeDrive(0, horizontalStick.getX());
+
+  
+ 
+  }
 
   @Override
   public void disabledInit() {}
